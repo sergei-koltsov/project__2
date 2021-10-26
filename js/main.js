e884839dfe0f3402d9b745c838bdde5ad42b7b88
sergei-koltsov <serhiikoltsov@icloud.com>
2021-10-26 14:07:38 +0300

    swiper?

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  var mixer = mixitup('.gallery__content');
+});
+
+var swiper = new Swiper(".blog__slider", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
